59c61321ce41dc09e88a89543570ce339c57b10a
Vishal Kashyap <147410216+vishalkashyap01@users.noreply.github.com>
2025-05-04 08:00:01 +0530

    change title

--- components/header.tsx
+++ components/header.tsx
@@ -31,21 +31,17 @@ export const Header = () => {
               </svg>
             </Link>
             <div className="jsx-e3e12cc6f9ad5a71 w-4 text-lg text-center text-zinc-300 dark:text-zinc-600">
-              <svg
-                data-testid="geist-icon"
-                height={16}
-                strokeLinejoin="round"
-                viewBox="0 0 16 16"
-                width={16}
-                style={{ color: "currentcolor" }}
-              >
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M4.01526 15.3939L4.3107 14.7046L10.3107 0.704556L10.6061 0.0151978L11.9849 0.606077L11.6894 1.29544L5.68942 15.2954L5.39398 15.9848L4.01526 15.3939Z"
-                  fill="currentColor"
-                />
-              </svg>
+             <svg
+  data-testid=""
+  height={16}
+  strokeLinejoin="round"
+  viewBox="0 0 16 16"
+  width={16}
+  style={{ color: "currentcolor" }}
+>
+  <title>Vishal</title>
+  {/* Your SVG path or content here */}
+</svg>
             </div>
             <div className="jsx-e3e12cc6f9ad5a71 flex flex-row items-center gap-4">
               <Link className="flex flex-row items-end gap-2" target="_blank" href="https://groq.com">
